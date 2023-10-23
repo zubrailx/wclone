031706b2ad95ce64392efb54f2ca550048c2656e
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-10-23 21:41:21 +0300

    move gdrive function separately

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createSignal, onMount, For } from "solid-js";
 import { loadScript } from "./util";
+import { listFiles } from "./gdrive";
 import './App.scss'
 
 function App() {
@@ -74,32 +75,8 @@ function App() {
     }
   }
 
-  async function listFiles() {
-    let response;
-    try {
-      response = await gapi.client.drive.files.list({
-        'pageSize': 10,
-        'fields': 'files(id, name)',
-      });
-    } catch (err) {
-      document.getElementById('content').innerText = err.message;
-      return;
-    }
-    const files = response.result.files;
-    if (!files || files.length == 0) {
-      document.getElementById('content').innerText = 'No files found.';
-      return;
-    }
-    const output = files.reduce(
-      (str, file) => `${str}${file.name} (${file.id})\n`,
-      'Files:\n');
-    document.getElementById('content').innerText = output;
-  }
-
   return (
     <>
-      <div>
-      </div>
       <h1>wclone</h1>
       <button id="authorize_button" onClick={handleAuthClick}>Authorize</button>
       <button id="signout_button" onClick={handleSignoutClick}>Sign Out</button>
